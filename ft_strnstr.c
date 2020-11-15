dc81eae55c51e56c415ff9bd910ba2a57ce791fa
tsaiyan <tsaiyan21@yandex.ru>
2020-11-15 13:36:16 +0300

    pushpush

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -5,28 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsaiyan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/30 16:18:09 by tsaiyan           #+#    #+#             */
-/*   Updated: 2020/10/30 16:18:10 by tsaiyan          ###   ########.fr       */
+/*   Created: 2020/11/15 13:35:43 by tsaiyan           #+#    #+#             */
+/*   Updated: 2020/11/15 13:35:44 by tsaiyan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t i;
+		 size_t	j;
+		 size_t	i;
 
-	i = 0;
-	if (!*needle)
-		return ((char*)haystack);
-	while(len-- && *haystack)
-	{
-		if (*haystack == *needle)
-			i++;
-		if (!needle || !len)
-			return ((char*)haystack);
-		haystack++;
-		needle++;
-	}
-	return (NULL);
-}
+		 if (!*needle)
+			 return ((char *)haystack);
+		 j = 0;
+		 while (haystack[j] && j < len)
+		 {
+			 i = 0;
+			 while (haystack[j + i] == needle[i] && j + i < len)
+			 {
+				 i++;
+				 if (!needle[i])
+					 return ((char *)haystack + j);
+			 }
+			 j++;
+		 }
+		 return (NULL);
+	 }
